203783bb6a6cb0ff50bb96e68a716bb276dce860
fellypsantos <fellypsantos2011@gmail.com>
2022-07-10 22:51:09 -0300

    finished module 03

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
+const config = require('config');
+const dbConfig = config.get('database');
+const {database, username, password, dialect, storage, logging} = dbConfig;
 
-const sequelize = new Sequelize('hoaxify', '', '', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false,
+const sequelize = new Sequelize(database, username, password, {
+  dialect,
+  storage,
+  logging,
 });
 
 module.exports = sequelize;
